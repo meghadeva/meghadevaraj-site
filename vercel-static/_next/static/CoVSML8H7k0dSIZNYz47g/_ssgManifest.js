b0aeabf3e4de6c72e96ebef8cdbc0e018730d827
Megha Devaraj <meghadevaraj@Mac.attlocal.net>
2026-05-26 17:10:46 -0700

    Publish static Moves demo

---
+++ vercel-static/_next/static/CoVSML8H7k0dSIZNYz47g/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fai\u002F[slug]","\u002Fdemos\u002F[slug]","\u002Feducation\u002F[slug]","\u002Fhobbies\u002F[slug]","\u002Fprojects\u002F[slug]","\u002Fwork\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
